4e7c817db54a0c46818dcb0f404b4f507a4ed70e
GoldenPavilion <rcorystine@gmail.com>
2021-07-22 11:51:03 -0400

    Done.

--- src/index.js
+++ src/index.js
@@ -1,3 +1,13 @@
 document.addEventListener("DOMContentLoaded", () => {
-  // your code here
+  const form = document.getElementById("create-task-form");
+  const taskList = document.getElementById("tasks");
+
+  form.addEventListener("submit", function(e){
+    e.preventDefault();
+    const newTask = document.querySelector("#new-task-description").value;
+    const item = document.createElement("li")
+    item.innerText = newTask;
+    taskList.appendChild(item);
+  });
 });
+
